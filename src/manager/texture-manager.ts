48f2e003afab0b2ceec90677d09ebfaf245e6f06
biud436 <biud436@gmail.com>
2023-01-22 23:17:01 +0900

    feat: 풀 사이즈 제한 기능 추가

--- src/manager/texture-manager.ts
+++ src/manager/texture-manager.ts
@@ -6,22 +6,45 @@ export interface ITextureManager {
     remove(id: string): boolean;
 }
 
+export class Texture2D {
+    id: string;
+    texture: HTMLImageElement;
+
+    constructor(id: string, texture: HTMLImageElement) {
+        this.id = id;
+        this.texture = texture;
+    }
+
+    static of(id: string, texture: HTMLImageElement): Texture2D {
+        return new Texture2D(id, texture);
+    }
+}
+
 export class TextureManager implements ITextureManager {
-    private textureMap: Map<string, HTMLImageElement> = new Map();
-    private onLoadedCallback: Map<string, (img: HTMLImageElement) => void> =
-        new Map();
+    private textureMap: Map<string, Texture2D> = new Map();
+    private onLoadedCallback: Map<string, (img: Texture2D) => void> = new Map();
     static MAX_POLL_SIZE: number = 20;
 
     load(filename: string, id: string): boolean {
+        if (this.textureMap.has(id)) {
+            return false;
+        }
+
+        if (!this.isValidPoolSize()) {
+            console.error("텍스쳐 풀 사이즈가 초과되었습니다.");
+            return false;
+        }
+
+        // Create image
         const image = new Image();
 
-        this.onLoadedCallback.set(id, (img: HTMLImageElement) => {
+        this.onLoadedCallback.set(id, (img: Texture2D) => {
             this.textureMap.set(id, img);
         });
 
         image.src = filename;
         image.onload = () => {
-            this.onLoadedCallback.get(id)?.(image);
+            this.onLoadedCallback.get(id)?.(Texture2D.of(id, image));
         };
 
         return true;
@@ -30,20 +53,23 @@ export class TextureManager implements ITextureManager {
     remove(id: string): boolean {
         const isRemoved = this.textureMap.delete(id);
 
-        if (this.onLoadedCallback.has(id)) {
-            this.onLoadedCallback.delete(id);
-        }
+        if (this.onLoadedCallback.has(id)) this.onLoadedCallback.delete(id);
 
         return isRemoved;
     }
 
+    /**
+     * Update listeners
+     */
     update(): void {
         this.onLoadedCallback.forEach((callback, id) => {
-            if (this.textureMap.has(id)) {
-                callback(this.textureMap.get(id)!);
-            }
+            if (this.textureMap.has(id)) callback(this.textureMap.get(id)!);
         });
 
         this.onLoadedCallback.clear();
+    }
+
+    isValidPoolSize(): boolean {
+        return this.textureMap.size < TextureManager.MAX_POLL_SIZE;
     }
 }
