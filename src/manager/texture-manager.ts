dac48656d7e5b67b8bd5c36e516d61e07701632b
biud436 <biud436@gmail.com>
2023-01-22 21:43:47 +0900

    feat: added TextureManager

--- src/manager/texture-manager.ts
+++ src/manager/texture-manager.ts
@@ -4,13 +4,30 @@ import { Rect } from "../core/rect";
 export interface ITextureManager {
     load(filename: string, id: string): boolean;
     remove(id: string): boolean;
-    drawText(
-        id: string,
-        x: number,
-        y: number,
-        width: number,
-        height: number,
-        rect: Rect,
-        transform: Matrix
-    ): void;
+}
+
+export class TextureManager implements ITextureManager {
+    private textureMap: Map<string, HTMLImageElement> = new Map();
+    private onLoadedCallback: Map<string, () => void> = new Map();
+
+    load(filename: string, id: string): boolean {
+        const image = new Image();
+        image.src = filename;
+        image.onload = () => {
+            this.onLoadedCallback.get(id)?.();
+        };
+        this.textureMap.set(id, image);
+
+        return true;
+    }
+
+    remove(id: string): boolean {
+        const isRemoved = this.textureMap.delete(id);
+
+        if (this.onLoadedCallback.has(id)) {
+            this.onLoadedCallback.delete(id);
+        }
+
+        return isRemoved;
+    }
 }
