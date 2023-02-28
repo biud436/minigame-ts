d57e21ed91fcfc46d0e8303b930c954b5e22fef6
biud436 <biud436@gmail.com>
2023-02-28 10:34:22 +0900

    fix: removed unused library

--- server/src/controllers/auth/strategies/jwt.strategy.ts
+++ server/src/controllers/auth/strategies/jwt.strategy.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { AuthService } from '../auth.service';
